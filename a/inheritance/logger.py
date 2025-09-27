d7dc791e331947b6fdb669c371278dfd6b342dec
sukhmani <sukhmani@bellevuecollege.edu>
2025-09-27 20:43:36 +0000

    encap

--- a/inheritance/logger.py
+++ a/inheritance/logger.py
@@ -1,21 +1,18 @@
-class Profile:
-    def __init__(self, email, password):
-       self.__email = email
-       self.__password = password
+class Book:
+    def __init__(self, title, author, pages):
+        self.__title = title
+        self.__author = author
+        self.__pages = pages
 
-    def setemail(self, email):
-        self.__email = email
+    def setpages(self, pages):
+        self.__pages = pages
+
+    def getpages(self):
+        return self.__pages
+    
+    def displayinfo(self):
+        return self.__title, self.__author, self.__pages
         
-    def chkpass(self, pwd):
-        return pwd == self.__password
-        
-    def displayemail(self):
-        return self.__email
+b = Book("a","b",1)
+print(b.displayinfo())
        
-
-p = Profile("sukhmani@example.com", "secure123")
-print(p.displayemail())
-print(p.chkpass("wrongpass"))
-print(p.chkpass("secure123"))
-p.setemail("newemail@example.com")
-print(p.displayemail())
